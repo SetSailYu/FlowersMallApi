a2273edc5e3fe3bc6f1959f47682408802732195
SetSailYu <1227322976@qq.com>
2020-06-14 13:42:37 +0800

    新增右侧推荐接口

--- FlowersMallApi/Controllers/ApiController.cs
+++ FlowersMallApi/Controllers/ApiController.cs
@@ -77,55 +77,16 @@ namespace FlowersMallApi.Controllers
         {
         }
 
-
-        // GET: api/<controller>/Index
+        /// <summary>
+        /// 轮播图接口
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/<controller>/ImgList
         [HttpGet]
-        public JsonResult Index()
+        public JsonResult ImgList()
         {
             var person = new
             {
-                classify = new {
-                    flower = new object[]{
-                        new{
-                            name = "鲜花系列",
-                            id = "xh",
-                            list = new string[]{ "爱情系列", "生日系列", "婚庆系列", "生活系列", "商务系列", "殡仪系列", "其它系列" }
-                        },
-                        new{
-                            name = "鲜花花材",
-                            id = "hc",
-                            list = new string[] { "玫瑰", "康乃馨", "百合", "向日葵", "扶郎", "郁金香", "马蹄莲" }
-                        }
-                    },
-                    immortal = new object[]{
-                        new{
-                            name = "永生花",
-                            id = "ys",
-                            cla = new string[] { "stature1", "M-baffle1", "rm1", "height:62px;width:96px;" },
-                            list = new object[] {
-                                new { name = "经典花盒", url = "//img02.hua.com/pc/pimg/ysh_brand_menu_01.jpg"},
-                                new { name = "巨型玫瑰", url = "//img02.hua.com/pc/pimg/ysh_brand_menu_02.jpg"},
-                                new { name = "薰衣草", url = "//img02.hua.com/pc/pimg/ysh_brand_menu_03.jpg"},
-                                new { name = "永生瓶花", url = "//img02.hua.com/pc/pimg/ysh_brand_menu_04.jpg"},
-                                new { name = "特色永生花", url = "//img02.hua.com/pc/pimg/ysh_brand_menu_05.jpg"}
-                            }
-                        },
-                        new{
-                            name = "礼品",
-                            id = "lp",
-                            cla = new string[] { "stature2", "M-baffle2", "rm2", "height:62px;width:58px;" },
-                            list = new object[] {
-                                new { name = "音乐盒", url = "//img02.hua.com/pc/assets/img/gifts/gifts-musicbox.png"},
-                                new { name = "金箔花", url = "//img02.hua.com/pc/assets/img/gifts/gifts-goldenflower.png"},
-                                new { name = "3D水晶内雕", url = "//img02.hua.com/pc/assets/img/gifts/gifts-crystallaser.png"},
-                                new { name = "首饰/美妆", url = "//img02.hua.com/pc/assets/img/gifts/gifts-shoushi.png"},
-                                new { name = "巧克力", url = "//img02.hua.com/pc/assets/img/gifts/gifts-chocolates.pngg"},
-                                new { name = "公仔/睡枕", url = "//img02.hua.com/pc/assets/img/gifts/gifts-toys.png"},
-                                new { name = "摆件/其他", url = "//img02.hua.com/pc/assets/img/gifts/gifts-dengshi.png"}
-                            }
-                        }
-                    }
-                },
                 imgList = new string[] {
                     "http://47.112.230.140:56000/images/1.jpg",
                     "http://47.112.230.140:56000/images/2.jpg",
@@ -133,10 +94,21 @@ namespace FlowersMallApi.Controllers
                     "http://47.112.230.140:56000/images/4.jpg",
                     "http://47.112.230.140:56000/images/5.jpg"
                 }
-
             };
 
             return Json(person);
+        }
+
+        /// <summary>
+        /// 右侧推荐接口
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/<controller>/Recommend
+        [HttpGet]
+        public IEnumerable<CommodityTable> Recommend()
+        {
+            //"select * from Commodity_Table where c_id=196 or c_id=195 or c_id=27"
+            return _context.CommodityTable.Where(u => u.CId == 27 || u.CId == 195 || u.CId == 196);
         }
 
     }
